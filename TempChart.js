93482399138b91784b167d1f603766a9d53ef480
Raygud <runigudmundarson@hotmail.com>
2022-03-17 14:17:10 +0100

    k

--- TempChart.js
+++ TempChart.js
@@ -7,14 +7,10 @@ let a = 1
 let q = 0
 x = 3
 let TempRange =[]  
-
-var date1 = new Date();
+let TempRangeMax
+let TempRangeMin
+let date1 = new Date();
      
-
-for (let i = 0; i < 300; i++) {
-  TempRange.push(300-i)
-  }
-
 function getGeoLocation() {
     // Navigator is your browser. 
   navigator.geolocation.getCurrentPosition(success => {
@@ -43,42 +39,125 @@ async function getWeatherData(latitude, longitude) {
     const labels = [date1.getHours()+":00"]
     for (let i = 0; i < 7; i++) {
     //  console.log(Weather.hourly[q].temp)
-        
      date1.setHours(date1.getHours()+2);
      Add0 = date1.getHours()
      Add0 = Add0.toString().length
 
      if(Add0 == 1){
-        console.log("0"+date1.getHours()+":"+date1.getMinutes())
-        labels.push("0"+date1.getHours()+":"+date1.getMinutes())
+        console.log("0"+date1.getHours()+":00")
+        labels.push("0"+date1.getHours()+":00")
      }
      else{
-        console.log(date1.getHours()+":"+date1.getMinutes())
+        console.log(date1.getHours()+":00")
         labels.push(date1.getHours()+":00")
      }
      
     q = q + 2
     }
+    for (let i = 0; i < 11; i++) {
+      TempRange.push(Weather.hourly[i].temp)
+      console.log(TempRange)
+      }
+      TempRange.sort(function(a, b){return a - b});
+      console.log(TempRange)
+      TempRangeMax = TempRange[TempRange.length-1]
+      TempRangeMin = TempRange[TempRange.length-TempRange.length]
+      console.log("Max Temp today: "+TempRangeMax)
+      console.log("Min Temp today: "+TempRangeMin)
+
     
 const ctx = document.getElementById("TempChart").getContext("2d")
+let Gradient = ctx.createLinearGradient(0,0,0, 220)
+Chart.defaults.font.size = 20;
+Chart.defaults.color = "#ffffff";
+
+
+switch (true) {
+  case (TempRangeMax <= 3):
+      Gradient.addColorStop(0, "rgba(131,238,255,0.9)")
+      Gradient.addColorStop(0.9, "rgba(131,238,255,0.8)")
+      Gradient.addColorStop(1, "rgba(255,255,255,0.7)")
+        break;
+    case (TempRangeMax <= 10):
+      Gradient.addColorStop(0, "rgba(255,255,51,0.9)")
+      Gradient.addColorStop(0.7, "rgba(255,255,51,0.8)")
+      Gradient.addColorStop(1, "rgba(131,238,255,0.7)")
+        break;
+    case (TempRangeMax <= 16):
+      Gradient.addColorStop(0, "rgba(253, 147, 8,0.9)")
+      Gradient.addColorStop(0.6, "rgba(255,255,51,0.8)")
+      Gradient.addColorStop(1, "rgba(255,255,51,0.7)")
+        break;
+    case (TempRangeMax <= 32):
+      Gradient.addColorStop(0, "rgba(252, 87, 94,0.9)")
+      Gradient.addColorStop(0.7, "rgba(247, 180, 44,0.8)")
+      Gradient.addColorStop(1, "rgba(253, 147, 8,0.7)")
+        break;
+        case (TempRangeMax <= 60):
+          Gradient.addColorStop(0, "rgba(252, 87, 94,0.9)")
+          Gradient.addColorStop(0.5, "rgba(252, 87, 94,0.8)")
+          Gradient.addColorStop(1, "rgba(228,0,120,0.7)")
+          break;
+    default:
+      Gradient.addColorStop(0, "rgba(253, 147, 8,0.9)")
+      Gradient.addColorStop(0.6, "rgba(255,255,51,0.8)")
+      Gradient.addColorStop(1, "rgba(255,255,51,0.7)")
+        break;
+}
+
+
+
+
+
+
 
 const Temps = {
     labels,
     datasets: [{
-        data: [Weather.hourly[0].temp,Weather.hourly[2].temp,Weather.hourly[4].temp,Weather.hourly[6].temp,Weather.hourly[8].temp,Weather.hourly[10].temp,Weather.hourly[12].temp,Weather.hourly[14].temp,]
+        data: [Weather.hourly[0].temp,Weather.hourly[2].temp,Weather.hourly[4].temp,Weather.hourly[6].temp,Weather.hourly[8].temp,Weather.hourly[10].temp,Weather.hourly[12].temp,Weather.hourly[14].temp]
+        
     }]
 }
 
-const config ={
-    type: "line",
-    data: Temps,
-    options:{
-        responsive:true,
-        lineTension: 0.4
-    },
-}
 
-const myChart = new Chart(ctx, config)
+const chart = new Chart(ctx, {
+  type: 'line',
+  data: Temps,
+  options: {
+    responsive:false,
+    lineTension: 0.4,
+    fill: true,
+    backgroundColor: Gradient,
+    plugins: {
+      legend: {
+        display: false
+      }
+    },
+      scales: {
+        x: {
+          grid: {
+            display:false
+          }
+        },
+          y: {
+            beginAtZero: true,
+            grid: {
+              display:false
+            },
+              ticks: {
+                  callback: function(value, index, ticks) {
+                      return  value + ' °C';
+                  }
+              }
+          }
+      }
+  }
+});
+
+for (let i = 0; i < 6; i++) {
+document.getElementById("Day"+i).setAttribute("href", "#"+Weather.daily[i].weather[0].main);
+console.log(Weather.daily[i].weather[0].main)
+}
 
 
 
--- test.js
+++
@@ -1,8 +0,0 @@
-let arr = []
-for (let i = 0; i < 10; i++) {
-arr.push(i)
-}
-arr[-1] = "a"
-arr[0] = "b"
-arr[1] = "c" 
-
